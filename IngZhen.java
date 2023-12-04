ac908c0392440f1df53ba3d976e7686811b3b509
JC Chuan <jc.chuan.0303@gmail.com>
2023-12-04 10:34:06 +0800

    added if statemet

--- IngZhen.java
+++ IngZhen.java
@@ -2,5 +2,8 @@ public class IngZhen {
     public static void main(String[] args) {
         System.out.println("Hello World LMAO!");
         System.out.println("This is edited here ");
+        if (true){
+            System.out.println("hmmmmmmmmm");
+        }
     }
 }
