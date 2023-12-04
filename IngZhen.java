bec4db277da46bf8d2e8085ae2079792217396d8
Jonas Chuan <99233005+PoisonDarterz@users.noreply.github.com>
2023-12-04 10:32:53 +0800

    edited println again

--- IngZhen.java
+++ IngZhen.java
@@ -1,6 +1,6 @@
 public class IngZhen {
     public static void main(String[] args) {
         System.out.println("Hello World LMAO!");
-        System.out.println("This is edited here ");
+        System.out.println("This is another line");
     }
 }
